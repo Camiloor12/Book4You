5a497ef8a8b7842b2f3897f5a03d423fe719e8d4
Camiloor12 <126282256+Camiloor12@users.noreply.github.com>
2023-10-11 17:24:46 +0200

    GG

--- src/Inicio.java
+++ src/Inicio.java
@@ -1,14 +1,3 @@
-import javax.swing.ImageIcon;
-import javax.swing.JButton;
-import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.JOptionPane;
-import javax.swing.JSeparator;
-
-
-import javax.swing.JPanel;
-
-import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Font;
 import java.awt.Graphics;
@@ -16,80 +5,91 @@ import java.awt.Image;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.FocusEvent;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.awt.event.FocusListener;
 
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+import com.toedter.calendar.JCalendar;
 
-	
-	
-	public class Inicio extends JFrame implements ActionListener {
-		
-		private static final long serialVersionUID = 1L;
-		
-		protected ImageIcon logo; 
-		protected JLabel logo2;
-		protected ImageIcon logout = new ImageIcon("logout.png");
-		protected JButton botonLogOut = new JButton(logout);
-		protected ImageIcon user= new ImageIcon("user.png");
-		protected JButton user2= new JButton(user);
-		protected ImageIcon image;
-		public Inicio () {
-			ImageIcon image = new ImageIcon("logot.jpg");
-			String imagenFondo = "fondoo.jpg";
-			Image fondo = new ImageIcon(imagenFondo).getImage();
+public class Inicio extends JFrame implements ActionListener {
 
-			setContentPane(new JPanel() {
-				@Override
-				protected void paintComponent(Graphics g) {
-					super.paintComponent(g);
-					this.setBackground(Color.white);
-					g.drawImage(fondo, 0, 150, getWidth(), 800, this);
-				}
-			});
-			
-			this.setDefaultCloseOperation(EXIT_ON_CLOSE);
-			this.setExtendedState(MAXIMIZED_BOTH);
-			this.setLayout(null);
-			
-			System.out.println("Existo!!");
-		
-			
-			
-			logo= new ImageIcon ("logo.png");
-			logo2= new JLabel(); 
-			logo2.setIcon(logo);
-			logo2.setBounds(725, -150, 500, 500);
-			botonLogOut.setBorderPainted(false);
-			botonLogOut.setOpaque(false);
-			botonLogOut.setContentAreaFilled(false);
-			botonLogOut.setBounds(20, 40, 100, 100);
-			botonLogOut.addActionListener(this);
-			user2.setBorderPainted(false);
-			user2.setOpaque(false);
-			user2.setContentAreaFilled(false);
-			user2.setBounds(1800,40,100,100);
-			user2.addActionListener(this);
-			
-			this.add(logo2);
-			this.add(botonLogOut);
-			this.add(user2);
-			this.setIconImage(image.getImage());
-			this.setVisible(true);
-		}
-		
-		
+    private ImageIcon logo;
+    private JLabel logo2;
+    private JLabel x;
+    private String x1= ("¿Dónde quieres ir?");
+    private ImageIcon logout = new ImageIcon("logout.png");
+    private JButton botonLogOut = new JButton(logout);
+    private ImageIcon user = new ImageIcon("user.png");
+    private JButton user2 = new JButton(user);
+    private ImageIcon image;
+    private JCalendar Calendario;
+    private JTextField Lugar;
+    private Font Fuente = new Font("Hammersmith One", Font.ITALIC, 120);
 
-		@Override
-		public void actionPerformed(ActionEvent e) {
-			if (e.getSource()== botonLogOut) {
-				this.dispose();
+    public Inicio() {
+        ImageIcon image = new ImageIcon("logot.jpg");
+    	String imagenFondo = "fondoo.jpg";
+		Image fondo = new ImageIcon(imagenFondo).getImage();
+
+		setContentPane(new JPanel() {
+			@Override
+			protected void paintComponent(Graphics g) {
+				super.paintComponent(g);
+				this.setBackground(Color.white);
+				g.drawImage(fondo, 0, 150, getWidth(), 800, this);
 			}
-			if (e.getSource()== user2) {
-				this.dispose();
-			}
-			
-		}
-		
-	}
+		});
+        this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        this.setExtendedState(MAXIMIZED_BOTH);
+        this.setLayout(null);
+        this.setTitle("BOOK4YOU");
 
+        System.out.println("Existo!!");
+
+        logo = new ImageIcon("logo.png");
+        logo2 = new JLabel();
+        logo2.setIcon(logo);
+        logo2.setBounds(725, -150, 500, 500);
+        botonLogOut.setBorderPainted(false);
+        botonLogOut.setOpaque(false);
+        botonLogOut.setContentAreaFilled(false);
+        botonLogOut.setBounds(20, 40, 100, 100);
+        botonLogOut.addActionListener(this);
+        user2.setBorderPainted(false);
+        user2.setOpaque(false);
+        user2.setContentAreaFilled(false);
+        user2.setBounds(1800, 40, 100, 100);
+        user2.addActionListener(this);
+        x = new JLabel("Elije tu nueva aventura...");
+        x.setForeground(Color.white);
+        x.setFont(Fuente);
+        x.setBounds(45, 250, 1500, 200);
+        Lugar = new JTextField();
+        Lugar.setForeground(Color.BLACK); 
+        Lugar.setBounds(45, 440, 400, 45);
+        Marcadores_de_Posicion.Cambio1(Lugar, "¿Dónde quieres ir?");
+
+       
+        this.add(logo2);
+        this.add(botonLogOut);
+        this.add(user2);
+        this.add(x);
+        this.add(Lugar);
+
+        this.setIconImage(image.getImage());
+        this.setVisible(true);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if (e.getSource() == botonLogOut) {
+            this.dispose();
+        }
+        if (e.getSource() == user2) {
+            this.dispose();
+        }
+    }}
---
+++ src/Marcadores_de_Posicion.java
@@ -0,0 +1,30 @@
+import javax.swing.JTextField;
+import java.awt.Color;
+import java.awt.event.FocusEvent;
+import java.awt.event.FocusListener;
+
+public class Marcadores_de_Posicion {
+
+    public static void Cambio1(JTextField caja, String palabra) {
+    	caja.setText(palabra);
+    	caja.setForeground(Color.GRAY);
+
+    	caja.addFocusListener(new FocusListener() {
+            @Override
+            public void focusGained(FocusEvent e) {
+                if (caja.getText().equals(palabra)) {
+                	caja.setText("");
+                	caja.setForeground(Color.BLACK);
+                }
+            }
+
+            @Override
+            public void focusLost(FocusEvent e) {
+                if (caja.getText().isEmpty()) {
+                	caja.setText(palabra);
+                	caja.setForeground(Color.GRAY);
+                }
+            }
+        });
+    }
+}
